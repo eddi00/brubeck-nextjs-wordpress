9831aed5815711480b3b6302f6b7b6b1dba3f286
ed <eduard.beliaev@gmail.com>
2022-06-14 02:08:16 +0500

    Update

--- src/components/Checkout.page/Checkout.component.jsx
+++ src/components/Checkout.page/Checkout.component.jsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import { useRouter } from "next/router";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 import { FlexWrapper, Layout } from "./Checkout.styles";
 import GuestForm from "./GuestForm/GuestForm.component";
 
 const Checkout = () => {
+  const router = useRouter();
+  const cartItemsCount = useSelector(state => selectCartItemsCount(state));
+  if (cartItemsCount === 0) router.push("/");
+
   return (
     <Layout>
       {/* <h2>Оформление заказа</h2> */}
