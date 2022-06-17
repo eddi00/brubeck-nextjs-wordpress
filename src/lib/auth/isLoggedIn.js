229f1da37fe47352a0a154274271fc90d68cbea0
ed <eduard.beliaev@gmail.com>
2022-06-17 10:08:05 +0500

    Remove build errors

--- src/lib/auth/isLoggedIn.js
+++ src/lib/auth/isLoggedIn.js
@@ -1,22 +1,22 @@
-// isLoggedIn.jsx
+// // isLoggedIn.jsx
 
-export default (GetServerSidePropsFunction) => async (ctx) => {
-  // 1. Check if there is a token in cookies. Let's assume that your JWT is stored in 'jwt'.
-  const token = ctx.req.cookies?.jwt || null;
+// export default (GetServerSidePropsFunction) => async (ctx) => {
+//   // 1. Check if there is a token in cookies. Let's assume that your JWT is stored in 'jwt'.
+//   const token = ctx.req.cookies?.jwt || null;
 
-  // 2. Perform an authorized HTTP GET request to the private API to check if the user is genuine.
-  const { data } = await authenticate(...); // your code here...
+//   // 2. Perform an authorized HTTP GET request to the private API to check if the user is genuine.
+//   const { data } = await authenticate(...); // your code here...
 
-  // 3. If there is no user, or the user is not authenticated, then redirect to homepage.
-  if (!data) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  }
+//   // 3. If there is no user, or the user is not authenticated, then redirect to homepage.
+//   if (!data) {
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  // 4. Return your usual 'GetServerSideProps' function.
-  return await GetServerSidePropsFunction(ctx);
-};
+//   // 4. Return your usual 'GetServerSideProps' function.
+//   return await GetServerSidePropsFunction(ctx);
+// };
