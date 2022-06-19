519f9431dd2ccd7a64b5bdcb3264567812a0d75f
ed <eduard.beliaev@gmail.com>
2022-06-19 07:37:17 +0500

    Favorites syncronization

--- src/components/GlobalWrapper/GlobalWrapper.component.jsx
+++ src/components/GlobalWrapper/GlobalWrapper.component.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import { getCartSaga } from "../../redux/cart/cart.slice";
 import { useDispatch, useSelector } from "react-redux";
+import { getFavoritesSaga } from "../../redux/favorites/favorites.slice";
 
 const GlobalWrapper = props => {
   const dispatch = useDispatch();
   const currentUser = useSelector(state => state.user.currentUser);
+
   useEffect(() => {
     if (currentUser) {
       dispatch(getCartSaga());
+      dispatch(getFavoritesSaga());
     }
   }, [currentUser]);
 
