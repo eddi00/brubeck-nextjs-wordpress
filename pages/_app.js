96714b8d7a3d1acbda8e88128d6bf8ff71c3cabb
ed <eduard.beliaev@gmail.com>
2022-06-20 16:23:42 +0500

    changed font display to block

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
+import Head from "next/head";
 
 import { store } from "../src/redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "../src/redux/store";
 
-import "normalize.css";
+//import "normalize.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
 import { ThemeProvider } from "styled-components";
--- pages/_document.js
+++ pages/_document.js
@@ -18,15 +18,15 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=block"
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Murecho:wght@100;200;300;400;500;600;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Murecho:wght@100;200;300;400;500;600;700;800&display=block"
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Open Sans:wght@300;400;500;600;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Open Sans:wght@300;400;500;600;700;800&display=block"
             rel="stylesheet"
           />
         </Head>
